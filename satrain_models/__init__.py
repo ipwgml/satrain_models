1c6e0ed61d5b5968bef0fc200f12cb49097dcb3a
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2025-12-18 09:03:17 -0500

    Remove SWinUNet for now.

--- satrain_models/__init__.py
+++ satrain_models/__init__.py
@@ -3,7 +3,6 @@
 from .config import ComputeConfig, SatRainConfig
 from .datamodule import SatRainDataModule
 from .lightning import SatRainEstimationModule
-from .swinunet import SwinUnet, create_swinunet
 from .tensorboard_to_netcdf import (
     extract_all_training_runs,
     extract_scalars_from_tensorboard,
@@ -18,7 +17,6 @@ __author__ = "SatRain Contributors"
 __all__ = [
     "UNet",
     "create_unet",
-    "SwinUnet",
     "create_swinunet",
     "SatRainConfig",
     "SatRainEstimationModule",
