54dfcec7baa33cec4abe5a3d5d7fa675f9b18a29
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2026-03-01 12:45:09 -0500

    Add config files for Swin UNet.

--- models/encoder_decoder/complexity_scaling.sh
+++ models/encoder_decoder/complexity_scaling.sh
@@ -1,8 +1,13 @@
-python train.py model_unet_xlarge.toml
-python train.py model_unet_large.toml
-python train.py model_unet_medium.toml
-python train.py model_unet_small.toml
-python train.py model_resnet_xlarge.toml
-python train.py model_resnet_large.toml
-python train.py model_resnet_medium.toml
-python train.py model_resnet_small.toml
+#python train.py model_unet_xlarge.toml
+#python train.py model_unet_large.toml
+#python train.py model_unet_medium.toml
+#python train.py model_unet_small.toml
+#
+#python train.py model_resnet_xlarge.toml
+#python train.py model_resnet_large.toml
+#python train.py model_resnet_medium.toml
+#python train.py model_resnet_small.toml
+
+python train.py model_swin_large.toml
+python train.py model_swin_medium.toml
+python train.py model_swin_small.toml
