ea10346db410223f25287ca0e1cbdc712c92ef14
Simon Pfreundschuh <s.pfreundschuh@gmail.com>
2026-04-23 11:20:30 -0600

    Add training scripts for other SatRain sensors.

---
+++ models/encoder_decoder/other_sensors.sh
@@ -0,0 +1,3 @@
+python train.py model_resnet_medium.toml --dataset-config dataset_geo_ir.toml
+python train.py model_resnet_medium.toml --dataset-config dataset_atms.toml
+python train.py model_resnet_medium.toml --dataset-config dataset_geo.toml
---
+++ models/xgboost/other_sensors.sh
@@ -0,0 +1,3 @@
+python train.py compute.toml --dataset-config dataset_geo_ir.toml
+python train.py compute.toml --dataset-config dataset_atms.toml
+python train.py compute.toml --dataset-config dataset_geo.toml
